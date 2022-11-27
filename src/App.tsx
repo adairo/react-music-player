ec3a75922d68fac70159a732774a7232d36da9eb
adairo <adair.reyes450@gmail.com>
2022-11-27 13:43:00 -0600

    change from fileList to playlist

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,6 @@ import { Tab } from "@headlessui/react";
 // import { read } from "jsmediatags";
 import emptyListIllustration from "../public/empty_list.svg";
 
-const songs = [
-  { title: "Zanzibar", artist: "Billy Joel", duration: "4:34" },
-  { title: "We are the champions", artist: "Queen", duration: "5:23" },
-  { title: "Heart of the sunrise", artist: "Yes", duration: "2:23" },
-  { title: "How does it feel", artist: "Toto", duration: "5:23" },
-  { title: "We are the champions", artist: "Queen", duration: "5:23" },
-  { title: "Heart of the sunrise", artist: "Yes", duration: "2:23" },
-  { title: "How does it feel", artist: "Toto", duration: "5:23" },
-];
-
 import {
   FolderIcon,
   ListBulletIcon,
@@ -21,11 +11,8 @@ import {
   PlusIcon,
 } from "@heroicons/react/20/solid";
 import { HeartIcon } from "@heroicons/react/24/outline";
-import { PictureType, TagType } from "jsmediatags/types";
+import { PictureType, Tags, TagType } from "jsmediatags/types";
 import { ReactNode, useEffect, useRef, useState } from "react";
-
-type SongList = typeof songs;
-type Song = SongList[number];
 
 function App() {
   return (
@@ -65,10 +52,52 @@ function App() {
   );
 }
 
+const tagsToRead = ["title", "artist", "picture", "album"] as const;
+type SelectedTags = Pick<TagType["tags"], typeof tagsToRead[number]>;
+
+type SongFile = Required<Omit<SelectedTags, "picture">> & {
+  file: File;
+  id: number;
+  cover: string;
+};
+
+async function getSongsFromFiles(fileList: FileList | null) {
+  const files = Array.from(fileList ?? []);
+  const jsmediatags = window.jsmediatags;
+
+  const filePromises: Promise<SongFile>[] = files.map((file, index) => {
+    return new Promise((resolve, reject) => {
+      new jsmediatags.Reader(file)
+        .setTagsToRead(tagsToRead as unknown as string[])
+        .read({
+          onSuccess(data) {
+            const { tags } = data;
+            const coverURL = getImageURLFromPicture(tags.picture);
+            const result = {
+              title: tags.title ?? "",
+              artist: tags.artist ?? "Unknow",
+              album: tags.album ?? "",
+              cover: coverURL ?? "",
+              file: file,
+              id: index,
+            };
+            resolve(result);
+          },
+          onError(error) {
+            reject({ error });
+          },
+        });
+    });
+  });
+
+  const playList = await Promise.all(filePromises);
+  return playList;
+}
+
 function MusicLibrary() {
   const filePickerRef = useRef<HTMLInputElement>(null);
   const audioRef = useRef<HTMLAudioElement>(null);
-  const [fileList, setFileList] = useState<File[]>([]);
+  const [playlist, setPlaylist] = useState<SongFile[]>([]);
   const [playingSong, setPlayingSong] = useState<File | null>(null);
 
   const handlePlaySong = (title: string, file: File) => {
@@ -86,15 +115,12 @@ function MusicLibrary() {
     audioRef.current.onload = (e) => URL.revokeObjectURL(mediaURL);
   };
 
-  const handleInputFileChange = () => {
+  const handleInputFileChange = async () => {
     if (!filePickerRef.current) return;
 
     const newFileList = filePickerRef.current.files;
-    if (newFileList?.length === 0) {
-      return setFileList([]);
-    }
-
-    setFileList(Array.from(filePickerRef.current.files ?? []));
+    const playList = await getSongsFromFiles(newFileList);
+    setPlaylist(playList);
   };
   return (
     <div className="">
@@ -123,14 +149,10 @@ function MusicLibrary() {
         />
       </header>
       <section className="space-y-4 p-4  ">
-        {fileList.map((file) => (
-          <SongFilePreview
-            onPlay={handlePlaySong}
-            key={file.name}
-            file={file}
-          />
+        {playlist.map((song) => (
+          <SongFilePreview onPlay={handlePlaySong} key={song.id} song={song} />
         ))}
-        {fileList.length === 0 && (
+        {playlist.length === 0 && (
           <div className="max-w-xs mx-auto">
             <img src={emptyListIllustration} alt="" className="" />
             <p className="text-lg text-slate-500 mt-6 px-2 font-semibold">
@@ -146,15 +168,10 @@ function MusicLibrary() {
           </div>
         )}
       </section>
-      <audio className="w-full" autoPlay controls ref={audioRef} />
+      <audio className="w-full rounded-full" autoPlay controls ref={audioRef} />
     </div>
   );
 }
-
-type SongFilePreviewProps = {
-  file: File;
-  onPlay: (title: string, file: File) => void;
-};
 
 function getImageURLFromPicture(picture: PictureType | undefined) {
   if (!picture) return "";
@@ -168,55 +185,27 @@ function getImageURLFromPicture(picture: PictureType | undefined) {
   return url;
 }
 
-const tagsToRead = ["title", "artist", "picture", "album"] as const;
-type SelectedTags = Pick<TagType["tags"], typeof tagsToRead[number]>;
+type SongFilePreviewProps = {
+  song: SongFile;
+  onPlay: (title: string, file: File) => void;
+};
 
-function SongFilePreview({ file, onPlay }: SongFilePreviewProps) {
-  const [metadata, setMetadata] = useState<SelectedTags | null>(null);
-  const imgRef = useRef<HTMLImageElement>(null);
-
-  const handleUpdateImage = (url: string) => {
-    if (!imgRef.current) {
-      throw new Error("Don't forget to set ref to the img element");
-    }
-
-    console.log("🚀 ~ file: App.tsx ~ line 159 ~ handleUpdateImage ~ url", url);
-    imgRef.current.src = url;
-  };
-
-  useEffect(() => {
-    const jsmediatags = window.jsmediatags;
-    new jsmediatags.Reader(file)
-      .setTagsToRead(tagsToRead as unknown as string[])
-      .read({
-        onSuccess(data) {
-          const { title, artist, picture, album } = data.tags;
-          setMetadata({ title, artist, album });
-          const imageURL = getImageURLFromPicture(picture);
-          handleUpdateImage(imageURL);
-        },
-        onError(error) {
-          console.error(error);
-        },
-      });
-  }, [file]);
-
+function SongFilePreview({ song, onPlay }: SongFilePreviewProps) {
   return (
     <div className="grid grid-cols-[auto_1fr_auto] gap-2 items-center group">
       <div className="w-14 aspect-square bg-slate-300 relative grid place-items-center group">
-        <img ref={imgRef} />
+        <img src={song.cover} />
         <PlayCircleIcon
-          onClick={() => onPlay(metadata?.title ?? "Unknow", file)}
+          onClick={() => onPlay(song.title, song.file)}
           className="w-full bg-slate-800 text-white hidden group-hover:[display:block] absolute transition-transform origin-center active:scale-90 opacity-70 active:opacity-70"
         />
       </div>
       <div>
         <div className="flex gap-2 items-center">
-          <p className="font-bold text-lg text-slate-800">{metadata?.title}</p>
+          <p className="font-bold text-lg text-slate-800">{song.title}</p>
         </div>
         <p>
-          {metadata?.artist} -{" "}
-          <span className="text-slate-500">{metadata?.album}</span>
+          {song.artist} - <span className="text-slate-500">{song.album}</span>
         </p>
       </div>
       <div className="hidden group-hover:block">
